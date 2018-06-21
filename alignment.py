fe08a052de9e273899e0be6a7b2b920cbde3d578
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2018-06-21 16:20:17 +0530

    word alignment

---
+++ alignment.py
@@ -0,0 +1,177 @@
+from collections import defaultdict
+import copy
+import itertools
+import operator
+from functools import reduce
+
+def em_run(sentence_pairs):
+
+    source_sentences, target_sentences = list(zip(*sentence_pairs))
+    
+    le = []
+    lh = []
+
+    for sentencesE in source_sentences:
+        for e in enumerate(sentencesE):
+            le.append(e)
+    
+    for sentencesH in target_sentences:
+        for h in enumerate(sentencesH):
+            lh.append(h)
+
+    print(le)
+    print(lh)
+    print ()
+    print()
+        
+    source_vocabulary = set(itertools.chain.from_iterable(source_sentences))
+    target_vocabulary = set(itertools.chain.from_iterable(target_sentences))
+
+    uniform_prob = 1.0 / len(source_vocabulary)
+
+    conditional_probs_old = None
+    conditional_probs = {(source_w, target_w): uniform_prob
+                         for source_w in source_vocabulary
+                         for target_w in target_vocabulary}
+
+    alignments = [[list(zip(source, target_perm))
+                   for target_perm in itertools.permutations(target)]
+                  for source, target in sentence_pairs]
+    
+
+    i = 0
+    while conditional_probs_old != conditional_probs:
+        conditional_probs_old = copy.copy(conditional_probs)
+
+        alignment_probs = {
+            i: {
+                tuple(alignment):
+                reduce(operator.mul, [conditional_probs[pair]
+                                      for pair in alignment])
+                for alignment in sentence_alignments
+            }
+
+            for i, sentence_alignments in enumerate(alignments)
+        }
+
+        for sentence_idx, sentence_alignments in alignment_probs.items():
+            total = float(sum(sentence_alignments.values()))
+            probs = {alignment: value / total
+                     for alignment, value in sentence_alignments.items()}
+            alignment_probs[sentence_idx] = probs
+
+
+        #source = []
+        #target = []
+        word_translations = defaultdict(lambda: defaultdict(float))
+        for sentence_alignments in alignment_probs.values():
+            for word_pairs, prob in sentence_alignments.items():
+                for source_word, target_word in word_pairs:
+                    word_translations[target_word][source_word] += prob
+
+        conditional_probs = {}
+        for target_word, translations in word_translations.items():
+            #target.append(target_word)
+            total = float(sum(translations.values()))
+            for source_word, score in translations.items():
+                #source.append(source_word)
+                conditional_probs[source_word, target_word] = score / total
+
+    wordP = []
+    trans = {}
+    
+    for source in source_vocabulary:
+        wordP = []
+        for target in target_vocabulary:
+            if (source, target) in conditional_probs:
+                wordP.append(conditional_probs[source, target])
+        maxP = max(wordP)
+        for target in target_vocabulary:
+            if (source, target) in conditional_probs:
+                if(conditional_probs[source, target] == maxP):
+                    trans.update({source : target})
+                    
+    print(trans)
+    print()
+    print()
+
+
+    posR = []
+    wordR = []
+    htrans = []
+    
+    for pos,word in le:
+        if word in trans:
+            posR.append(pos)
+            wordR.append(word)
+            htrans.append(trans[word])
+
+    print()
+    print()
+    print(posR)
+    print()
+    print(wordR)
+    print()
+    print(htrans)
+
+    hpos = []
+
+    for h in htrans:
+        for pos, word in lh:
+            if (h == word):
+                hpos.append(pos)
+                break
+
+
+    print()
+    print(hpos)
+
+
+    print()
+    print()
+    aligned = zip(posR, hpos)
+    print(list(aligned))
+    
+
+    #for pos, word in lh:
+     #   if word in htrans:
+      #      print(pos, word)
+        
+    
+    
+    #for target_word, translations in word_translations.items():
+        #for source_word, score in translations.items():
+            #print(translations.items())
+            #wordP = []
+            #while(len(translations) != len(wordP)):
+                #wordP.append(conditional_probs[source_word, target_word])
+            
+
+
+            #print(source_word, target_word)
+
+
+
+
+            #for maxProb in conditional_probs[source_word, target_word]:
+                #maxP = max(maxProb.items(), key = operator.itemgetter(1))[0]
+    print()
+    print()
+    return conditional_probs
+
+
+def main():
+
+    Elines = [Eline.rstrip('\n') for Eline in open('in.txt')]
+
+    with open('out.txt', 'r', encoding = 'utf-8') as hindi:
+        Hlines = [Hline.rstrip('\n') for Hline in hindi]
+        
+    SENTENCES = [(Elines[0].split(), Hlines[0].split()),
+                 (Elines[1].split(), Hlines[1].split()),
+                 (Elines[2].split(), Hlines[2].split())]
+
+    print (em_run(SENTENCES))
+
+if __name__ == '__main__':
+    main()
